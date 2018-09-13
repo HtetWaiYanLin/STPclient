a74a6a77ba16ff6c7f322ab9b2d8d378919bf457
Htet Wai Yan Lin <robetwaiyan@gmail.com>
2018-09-13 14:35:46 +0630

    Role UI and User UI Modify

--- src/app/system/user/user.component.ts
+++ src/app/system/user/user.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+
+  minDate = new Date(2000, 0, 1);
+  maxDate = new Date(2020, 0, 1);
+
+  master = false;
+  admin = false;
+  content_writer = false;
 
   constructor() { }
 
